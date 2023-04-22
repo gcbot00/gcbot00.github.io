8344e4764e7d3ac68df48889b7d51e0bee6ee4a1
gcbot00 <131486397+gcbot00@users.noreply.github.com>
2023-04-22 21:23:56 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ usercard.appendChild(p)
 btn1.addEventListener("click", function(){
     p.innerText = '1111'
     usercard.appendChild(p)
-    window.location='http://gcbot00.github.io/test';
 });
 
 btn2.addEventListener("click", function(){
