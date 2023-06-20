a9d201bbaf36ed3efb05c645b9981c01718b00a3
Uni <holandsol6@gmail.com>
2023-06-20 22:10:20 +0400

    asda

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 let tg = window.Telegram.WebApp;
-
 tg.expand();
+
+let pg_forever = document.getElementById("buy-pg-forever");
+let pg_month = document.getElementById("buy-pg-month");
+
+pg_forever.addEventListener("click", () => {
+    tg.sendData(1)
+    tg.close()
+})
+
+// Telegram.WebApp.onEvent("mainButtonClicked", function(){
+//     tg.sendData()
+// })
