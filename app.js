ae0a390613675a934333e303a0fa37f44e0373a7
Uni <holandsol6@gmail.com>
2023-06-22 07:05:13 +0400

    s

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ tg.expand();
 
 let pg_forever = document.getElementById("buy-pg-forever");
 let pg_month = document.getElementById("buy-pg-month");
+let ttp = document.getElementById("buy-ttp");
+let sa = document.getElementById("buy-sa");
+let donate = document.getElementById('donate')
 
 pg_forever.addEventListener("click", () => {
-    tg.sendData('asdsad')
+    tg.sendData('premium-gold-forever')
     tg.close()
 })
 
-// Telegram.WebApp.onEvent("mainButtonClicked", function(){
-//     tg.sendData()
-// })
+pg_month.addEventListener("click", () => {
+    tg.sendData('premium-gold-month')
+    tg.close()
+})
+
+ttp.addEventListener("click", () => {
+    tg.sendData('tiktok-premium')
+    tg.close()
+})
+
+sa.addEventListener("click", () => {
+    tg.sendData('slivarmy')
+    tg.close()
+})
+
+donate.addEventListener("click", () => {
+    tg.sendData('slivarmy')
+    tg.close()
+})
